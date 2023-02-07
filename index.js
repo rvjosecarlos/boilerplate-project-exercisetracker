0b63916ad4d719a033b3346359fdeb572b725a35
Carlos <rvjosecarlos@gmail.com>
2023-02-07 02:08:26 -0600

    Correcciones finales

--- index.js
+++ index.js
@@ -2,23 +2,24 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose');
-const { MONGODB } = require('dotenv').config().parsed;
+//const { MONGODB } = require('dotenv').config().parsed;
+const MONGODB = process.env['MONGODB'];
 const bodyParser = require('body-parser');
 
 // CONEXION A LA BD Mongo
 (
-async function(){
+  async function() {
 
-  try{
-    await mongoose.connect( MONGODB, { useNewUrlParser: true, useUnifiedTopology: true } );
-  }
-  catch( error ){
-    console.error(error);
-  };
-})();
+    try {
+      await mongoose.connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true });
+    }
+    catch (error) {
+      console.error(error);
+    };
+  })();
 
 // DEFINE un esquema para los documentos
-const usuariosEsquema = new mongoose.Schema( {
+const usuariosEsquema = new mongoose.Schema({
 
   username: {
     type: String,
@@ -48,7 +49,7 @@ const usuariosEsquema = new mongoose.Schema( {
 });
 
 // MODELO que representa la coleccion de documentos en mongo de acuerdo al esquema
-const Usuarios = mongoose.model( 'Usuarios', usuariosEsquema );
+const Usuarios = mongoose.model('Usuarios', usuariosEsquema);
 
 app.use(cors())
 app.use(express.static('public'))
@@ -57,41 +58,41 @@ app.get('/', (req, res) => {
 });
 
 // Habilitar post desde el formulario
-app.use( bodyParser.urlencoded( { extended: true } ) );
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Define un router para las peticiones post y get al mismo path para NUEVOS USUARIOS
 app
-  .route( '/api/users' )
-  .get( obtenerUsuarios )
-  .post( agregarUsuario );
+  .route('/api/users')
+  .get(obtenerUsuarios)
+  .post(agregarUsuario);
 
 
 // Middlerware del manjeador GET '/api/users' consulta de todos los usuarios
-async function obtenerUsuarios( req, res ){
+async function obtenerUsuarios(req, res) {
 
-  try{
+  try {
 
-    const usuarios = await Usuarios.find().select( {
+    const usuarios = await Usuarios.find().select({
       _id: 1,
       username: 1,
       __v: 1
     });
-    res.json( usuarios );
+    res.json(usuarios);
 
   }
-  catch( error ){
-    console.error( error );
+  catch (error) {
+    console.error(error);
   }
 
 }
 
 
 // Middleware del manejador POST '/api/users' para ingresar un nuevo usuario
-async function agregarUsuario( req, res ){
+async function agregarUsuario(req, res) {
   const { username } = req.body;
-  
-  if( !username ){
-    res.json( { error: 'username is required' } );
+
+  if (!username) {
+    res.json({ error: 'username is required' });
     return;
   };
 
@@ -101,13 +102,13 @@ async function agregarUsuario( req, res ){
     log: []
   };
 
-  try{
-    const usuarioCreado = await Usuarios.create( nuevoUsuario );
+  try {
+    const usuarioCreado = await Usuarios.create(nuevoUsuario);
 
-    console.log( usuarioCreado );
-    res.json( { username: usuarioCreado.username, _id: usuarioCreado._id } );
+    //console.log( usuarioCreado );
+    res.json({ username: usuarioCreado.username, _id: usuarioCreado._id });
   }
-  catch( error ){
+  catch (error) {
     console.error(error);
   }
 
@@ -115,10 +116,10 @@ async function agregarUsuario( req, res ){
 
 // Define el manejador de peticiones post para ingresar EJERCICIOS
 
-app.post( '/api/users/:_id/exercises', agregarTarea );
+app.post('/api/users/:_id/exercises', agregarTarea);
 
 // Define el middleware para agregar nuevas tareas EJERCICIOS
-async function agregarTarea( req, res ){
+async function agregarTarea(req, res) {
   const { _id } = req.params;
   const { description, duration, date } = req.body;
   const errorObj = { error: 'Invalid _id' };
@@ -126,85 +127,85 @@ async function agregarTarea( req, res ){
 
   // Obten la fecha separada para crear la fecha con el dia correcto
   let fecha;
-  if( date ){
-    fecha = date;
+  if (date) {
+    fecha = (date);
   }
-  else{
+  else {
     fecha = new Date();
-  };  
+  };
   console.log('FECHAS',date, fecha);
 
 
-  try{
-    existeUsuario = await Usuarios.findOne( { _id } );
+  try {
+    existeUsuario = await Usuarios.findOne({ _id });
     //console.log('EXISTE', existeUsuario);
 
     const nuevaTarea = {
       description: description,
       duration: parseInt(duration),
-      date: new Date( fecha ).toDateString()
+      date: new Date(fecha).toDateString()
     };
-  
-    console.log( 'OBJ NUEVA TAREA',nuevaTarea );
-    existeUsuario.count++;
-    existeUsuario.log.push( nuevaTarea );
-  
-    const usuarioActualizado = await existeUsuario.save();
-  
-    console.log(' ULTIMO REGISTRO' ,usuarioActualizado.log[ usuarioActualizado.log.length - 1 ]);
 
+    //console.log( 'OBJ NUEVA TAREA',nuevaTarea );
+    existeUsuario.count++;
+    existeUsuario.log.push(nuevaTarea);
+
+    const usuarioActualizado = await existeUsuario.save();
+
+    //console.log(' ULTIMO REGISTRO' ,usuarioActualizado.log[ usuarioActualizado.log.length - 1 ]);
+
+    console.log('TAREA NUEVA');
     const { username } = usuarioActualizado;
-  
-    res.json( {
+
+    res.json({
       _id,
       username,
-      date: new Date( fecha ).toDateString(),
+      date: new Date(fecha).toDateString(),
       duration: nuevaTarea.duration,
       description: nuevaTarea.description
-    } );
+    });
 
   }
-  catch( error ){
-    res.json( errorObj );
+  catch (error) {
+    res.json(errorObj);
     return;
   };
 
-  if( !existeUsuario ){
-    res.json( errorObj );
+  if (!existeUsuario) {
+    res.json(errorObj);
     return;
   };
 
 }
 
 // Define el manejador para peticiones
-app.get( '/api/users/:id/logs', obtenerTareas );
+app.get('/api/users/:id/logs', obtenerTareas);
 
 // Middleware del manejador get para OBTENER TAREAS
-async function obtenerTareas( req, res ){
+async function obtenerTareas(req, res) {
 
   const { id } = req.params;
 
   const { from, to, limit } = req.query;
 
-  try{
+  try {
 
-    const usuario = await Usuarios.findById(  id  );
+    const usuario = await Usuarios.findById(id);
     const { _id, username, count, log } = usuario;
 
-    if( from && to && limit ){
-      
+    if (from && to && limit) {
 
-      const desdeSF = formatearFecha( from );
-      const hastaSF = formatearFecha( to );
+
+      const desdeSF = formatearFecha(from);
+      const hastaSF = formatearFecha(to);
 
       const desde = new Date(desdeSF);
       const hasta = new Date(hastaSF);
       const limite = parseInt(limit);
 
-      console.log('FEHCAS', desde, hasta);
-                                   
-      const filtroTareas = log.filter( (tarea, indice) => tarea.date > desde && tarea.date < hasta && indice < limite )
-                              .map( tarea => {
+      
+      const filtroTareas = log.filter((tarea, indice) => tarea.date >= desde && tarea.date <= hasta && indice <= limite)  
+                              .map(tarea => {
                                 const { description, duration, date } = tarea;
                                 return {
                                   description,
@@ -213,7 +214,7 @@ async function obtenerTareas( req, res ){
                                 }
                               });
 
-      console.log( filtroTareas );
+      //console.log( filtroTareas );
 
       res.json({
         _id,
@@ -225,18 +226,18 @@ async function obtenerTareas( req, res ){
       });
       return;
     }
-    else if( limit !== '0' ){
-      console.log('Entra', limit)
+    else if (limit !== '0' && limit) {
+      //console.log('Entra', limit)
       const limite = parseInt(limit);
-      const limiteArr = log.filter( ( tarea, indice ) => indice <= limite )
-                           .map( tarea => {
-                            const { description, duration, date } = tarea;
-                            return {
-                              description,
-                              duration,
-                              date: date.toDateString()
-                            }
-                           });
+      const limiteArr = log.filter((tarea, indice) => indice < limite)
+        .map(tarea => {
+          const { description, duration, date } = tarea;
+          return {
+            description,
+            duration,
+            date: date.toDateString()
+          }
+        });
 
       res.json({
         _id,
@@ -245,18 +246,19 @@ async function obtenerTareas( req, res ){
         log: limiteArr
       });
       return;
-    }     
+    }
 
-    console.log(usuario);
+    //console.log(usuario);
 
-    const filtroTareas = log.map( tarea => {
-                                const { description, duration, date } = tarea;
-                                return {
-                                  description,
-                                  duration,
-                                  date: date.toDateString()
-                                }
-                              });
+    const filtroTareas = log.map(tarea => {
+      const { description, duration, date } = tarea;
+      return {
+        description,
+        duration,
+        date: date.toDateString()
+      }
+    });
+    //console.log('COMPLETAS',filtroTareas);
 
     res.json({
       _id,
@@ -266,17 +268,17 @@ async function obtenerTareas( req, res ){
     });
 
   }
-  catch( error ){
-    res.json( { error: 'Invalid id' } );
+  catch (error) {
+    res.json({ error: 'Invalid id' });
   }
 
 }
 
-function formatearFecha( date ){
-  const datosFecha = date.match( /[0-9]{2,4}/g );
+function formatearFecha(date) {
+  const datosFecha = date.match(/[0-9]{2,4}/g);
   const year = datosFecha[0];
   const mes = datosFecha[1];
-  const dia = datosFecha[2] > 8 ? parseInt(datosFecha[2]) + 1 : '0'+(parseInt(datosFecha[2]) + 1);
+  const dia = datosFecha[2] > 8 ? parseInt(datosFecha[2]) + 1 : '0' + (parseInt(datosFecha[2]) + 1);
   return `${year}-${mes}-${dia}`;
 }
 
